3b424a9f19639ee2844902d372b1df182de2b563
Alessandro <giochialessandro35@gmail.com>
2022-12-15 01:19:05 +0100

    Aggiungere i file di progetto.

---
+++ SpawnMessage/Config.cs
@@ -0,0 +1,39 @@
+﻿namespace SpawnMessage
+{
+    using PlayerRoles;
+    using System.Collections.Generic;
+    using System.ComponentModel;
+
+    public class Config
+    {
+        [Description("The message type, broadcast / hint")]
+        public SpawnMessage.MessageType SpawnMessageType = SpawnMessage.MessageType.Hint;
+
+        [Description("The message to send when a player spawn.")]
+        public Dictionary<RoleTypeId, string> Message { get; set; } = new Dictionary<RoleTypeId, string>
+        {
+            [RoleTypeId.ClassD] = "You're <#FF8E00>Class-D Personnel</color>.",
+            [RoleTypeId.Scientist] = "You're <#FFFF7C>Scientist</color>.",
+            [RoleTypeId.FacilityGuard] = "You're <#5B6370>Facility Guard</color>.",
+            [RoleTypeId.Scp049] = "You're <#EC2121>SCP-049</color>.",
+            [RoleTypeId.Scp0492] = "You're <#EC2121>SCP-049-2</color>.",
+            [RoleTypeId.Scp079] = "You're <#EC2121>SCP-079</color>.",
+            [RoleTypeId.Scp096] = "You're <#EC2121>SCP-096</color>.",
+            [RoleTypeId.Scp106] = "You're <#EC2121>SCP-106</color>.",
+            [RoleTypeId.Scp173] = "You're <#EC2121>SCP-173</color>.",
+            [RoleTypeId.Scp939] = "You're <#EC2121>SCP-939</color>.",
+            [RoleTypeId.NtfPrivate] = "You're <#6FC3FF>Nine-Tailed Fox Private</color>.",
+            [RoleTypeId.NtfSergeant] = "You're <#0096FF>Nine-Tailed Fox Sergeant</color>.",
+            [RoleTypeId.NtfCaptain] = "You're <#003ECA>Nine-Tailed Fox Captain</color>.",
+            [RoleTypeId.ChaosConscript] = "You're <#008F1E>Chaos Insurgency Conscript</color>.",
+            [RoleTypeId.ChaosRifleman] = "You're <#008F1E>Chaos Insurgency Rifleman</color>.",
+            [RoleTypeId.ChaosMarauder] = "You're <#0A7D34>Chaos Insurgency Marauder</color>.",
+            [RoleTypeId.ChaosRepressor] = "You're <#006728>Chaos Insurgency Repressor</color>.",
+            [RoleTypeId.Spectator] = "You're <#D2D4C1>Spectator</color>.",
+            [RoleTypeId.Tutorial] = "You're <#FC00AB>TUTORIAL</color>.",
+        };
+
+        [Description("The seconds of the message.")]
+        public float MessageTime = 15;
+    }
+}
---
+++ SpawnMessage/SpawnMessage.cs
@@ -0,0 +1,40 @@
+﻿namespace SpawnMessage
+{
+    using MEC;
+    using PlayerRoles;
+    using PluginAPI.Core;
+    using PluginAPI.Core.Attributes;
+    using PluginAPI.Enums;
+    using PluginAPI.Events;
+
+    public class SpawnMessage
+    {
+        public enum MessageType
+        {
+            Broadcast,
+            Hint
+        };
+
+        [PluginEntryPoint("SpawnMessage", "1.0.0", "Send a Broadcast or Hint when a player spawn.", "Bay")]
+        void Enabled() => EventManager.RegisterEvents(this);
+
+        [PluginEvent(ServerEventType.PlayerSpawn)]
+        void OnPlayerSpawn(Player player, RoleTypeId roleTypeId)
+        {
+            Timing.CallDelayed(1, () =>
+            {
+                if (Config.Message.TryGetValue(roleTypeId, out string message))
+                {
+                    switch (Config.SpawnMessageType)
+                    {
+                        default: player.ReceiveHint(message, Config.MessageTime); break;
+                        case MessageType.Broadcast: player.SendBroadcast(message, (ushort)Config.MessageTime); break;
+                    }
+                }
+            });
+        }
+
+        [PluginConfig] 
+        public Config Config;
+    }
+}
